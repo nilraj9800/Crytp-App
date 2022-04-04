78af675591a1a8bd20e3be16b06acd9922d29046
nilraj9800 <1122nilraj@gmail.com>
2022-04-04 20:24:24 +0200

    Added index.html

--- App.js
+++ App.js
@@ -5,11 +5,11 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>check react</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
